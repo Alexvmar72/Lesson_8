7cc081fbb65a2a7dfaffdefeedf58a70ec9237ea
Alex Mark <alexvmar@yandex.ru>
2023-03-16 10:09:08 +0300

    change file4 homework8

--- task_8_4/Program.cs
+++ task_8_4/Program.cs
@@ -5,4 +5,55 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)             */
 
+using System;
+using static System.Console;
+Clear();
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num_result = int.Parse(Console.ReadLine()!);
+    return num_result;
+}
+
+int[,,] GetArray(int m)
+{
+    int[,,] result = new int[m, m, m];
+    int x = 10;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int e = 0; e < m; e++)
+            {
+                result[i, j, e] = x;
+                x++;
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int e = 0; e < array.GetLength(2); e++)
+            {
+                Write($"{array[i, j, e]}({i},{j},{e}) ");
+            }
+            WriteLine();
+        }
+        WriteLine();
+    }
+}
+
+int m = Prompt("Введите размер массива не больше 4: ");
+if (m > 4) WriteLine($"Введите размер массива не больше 4");
+else
+{
+    int[,,] array = GetArray(m);
+    PrintArray(array);
+}
