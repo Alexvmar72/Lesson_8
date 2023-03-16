81b39dfc1487d384b2eab8da046b48766593749e
Alex Mark <alexvmar@yandex.ru>
2023-03-16 09:35:22 +0300

    change file3 homework8

--- task_8_3/Program.cs
+++ task_8_3/Program.cs
@@ -1,10 +1,80 @@
-﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-Например, даны 2 матрицы:
-2 4 | 3 4
-3 2 | 3 3
-Результирующая матрица будет:
-18 20
-15 18              */
+﻿/*  Задача 58: Задайте две матрицы. 
+Напишите программу, которая будет находить произведение двух матриц.
+Например, заданы 2 массива:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+и
+1 5 8 5
+4 9 4 2
+7 2 2 6
+2 3 4 7
+Их произведение будет равно следующему массиву:
+1 20 56 10
+20 81 8 6
+56 8 4 24
+10 6 24 49  */
 
+using System;
+using static System.Console;
+Clear();
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num_result = int.Parse(Console.ReadLine()!);
+    return num_result;
+}
 
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] MultiplicacionNumber(int[,] array1, int[,] array2)
+{
+    int k = array1.GetLength(0);
+    int l = array1.GetLength(1);
+    int[,] result = new int[k, l];
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+
+            result[i, j] = array1[i, j] * array2[i, j];
+        }
+    }
+    return result;
+}
+
+int m = Prompt("Введите размер массивов по строкам: ");
+int n = Prompt("Введите размер массивов по столбцам: ");
+int[,] array1 = GetArray(m, n);
+int[,] array2 = GetArray(m, n);
+
+PrintArray(array1);
+WriteLine();
+PrintArray(array2);
+WriteLine();
+PrintArray(MultiplicacionNumber(array1, array2));
