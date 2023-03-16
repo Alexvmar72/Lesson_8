7b3c27665a7f933662fb68569819d1a24632606a
Alex Mark <alexvmar@yandex.ru>
2023-03-16 08:50:59 +0300

    change file1 homework8

--- task_8_1/Program.cs
+++ task_8_1/Program.cs
@@ -8,4 +8,67 @@
 9 5 3 2
 8 4 4 2                    */
 
+using System;
+using static System.Console;
+Clear();
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num_result = int.Parse(Console.ReadLine()!);
+    return num_result;
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+
+    return array;
+}
+
+int m = Prompt("Введите размер массива по строкам: ");
+int n = Prompt("Введите размер массива по столбцам: ");
+int[,] array = GetArray(m, n, 0, 9);
+
+PrintArray(array);
+WriteLine();
+PrintArray(SortArray(array));
