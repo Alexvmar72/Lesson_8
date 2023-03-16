4c528618fc114fe2b9c7782601d35b5990f94503
Alex Mark <alexvmar@yandex.ru>
2023-03-16 15:19:30 +0300

    change file5 homework8

--- task_8_5/Program.cs
+++ task_8_5/Program.cs
@@ -5,4 +5,84 @@
 11 16 15 06
 10 09 08 07 */
 
+using System;
+using static System.Console;
+Clear();
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num_result = int.Parse(Console.ReadLine()!);
+    return num_result;
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    int x = 1;
+    int i = 0;
+    int j;
+    int a = 0;
+    int b = 0;
+    int index = 1;
+
+    while (index < result.GetLength(0) * result.GetLength(1))
+    {
+
+        for (j = b; j < n; j++)
+        {
+
+            result[i, j] = x;
+            x++;
+            index++;
+        }
+
+        for (i = a + 1, j--; i < m; i++)
+        {
+            result[i, j] = x;
+            x++;
+            index++;
+        }
+        for (j = n - 2, i--; j >= b; j--)
+        {
+            result[i, j] = x;
+            x++;
+            index++;
+        }
+        for (i = m - 2, j++; i > a; i--)
+        {
+            result[i, j] = x;
+            x++;
+            index++;
+        }
+
+        i++;
+        a++;
+        b++;
+        m = m - 1;
+        n = n - 1;
+        index++;
+    }
+
+
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]}", 4);
+        }
+        WriteLine();
+    }
+}
+
+
+int m = Prompt("Введите размер массива по строкам: ");
+int n = Prompt("Введите размер массива по столбцам: ");
+int[,] array = GetArray(m, n);
+
+PrintArray(array);
