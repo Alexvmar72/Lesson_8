bc5f5604ad490edd2af8f5de1cf5e869181b0010
Alex Mark <alexvmar@yandex.ru>
2023-03-16 09:17:58 +0300

    change file2 homework8

--- task_8_2/Program.cs
+++ task_8_2/Program.cs
@@ -12,4 +12,66 @@
 
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка            */
 
+using System;
+using static System.Console;
+Clear();
 
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num_result = int.Parse(Console.ReadLine()!);
+    return num_result;
+}
+
+int[,] GetArray(int m)
+{
+    int[,] result = new int[m, m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result[i, j] = new Random().Next(10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int CalculatorSumArray(int[,] array)
+{
+    int sum = 0;
+    int minsum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < minsum || minsum == 0)
+        {
+            minsum = sum;
+            sum = 0;
+            result = i + 1;
+        }
+    }
+    return result;
+}
+
+int m = Prompt("Введите размер массива: ");
+int[,] array = GetArray(m);
+
+PrintArray(array);
+WriteLine();
+WriteLine($"Номер строки с наименьшей суммой элементов: {CalculatorSumArray(array)}");
